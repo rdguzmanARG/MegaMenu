bf9ece7a2b0e229010d1a745bfe965c4e9144bbb
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2026-03-30 13:18:19 -0300

    Refactoring styles

--- src/vue-components/IconMenu.vue
+++ src/vue-components/IconMenu.vue
@@ -15,51 +15,12 @@ export default {
 </script>
 
 <template>
-  <div @mouseenter="toggleMenu" class="mmenu-item-desktop mm-icon">
+  <div @mouseenter="toggleMenu" class="mm-item-desktop mm-icon">
     <i :class="iconClass"></i>
     <a :href="url" target="_blank">{{ label }}</a>
   </div>
-  <div @click="toggleMenu" class="mmenu-item-mobile mm-icon">
+  <div @click="toggleMenu" class="mm-item-mobile mm-icon">
     <i :class="iconClass"></i>
     <a :href="url" target="_blank">{{ label }}</a>
   </div>
 </template>
-
-<style>
-.mmenu-item-mobile {
-  display: none;
-}
-
-.mmenu-item-desktop {
-  display: flex;
-}
-
-@media screen and (max-width: 1024px) {
-  .mmenu-item-mobile {
-    display: flex;
-  }
-
-  .mmenu-item-desktop {
-    display: none;
-  }
-}
-
-.mm-icon {
-  cursor: pointer;
-  width: fit-content;
-  padding: 10px;
-  color: #666666;
-  gap: 10px;
-  position: relative;
-  align-items: center;
-
-  a {
-    text-decoration: none;
-    color: #000;
-  }
-
-  &.home {
-    color: #ff8300;
-  }
-}
-</style>
